a7f647bcd25094087cb1ed504d814095043a7ede
Mirko Kämpf <mirko.kaempf@gmail.com>
2023-12-30 09:28:24 +0100

    added opi-bot submodule

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -1,1 +1,7 @@
-init-venv.sh
+source ./init-venv.sh
+
+pip install -r ./../requirements.txt
+
+#cd assisstent
+#git submodule add https://github.com/kamir/opi-bot-poc
+#git submodule update --init --recursive
--- bin/init-venv.sh
+++ bin/init-venv.sh
@@ -1,4 +1,10 @@
-python -m venv ./../venvpbbp
-source ./../venvpbbp/bin/activate
+#
+# Define your Python environment you want to use.
+#
+#  You can use Anaconda to create a virtual environment. Open it in a Terminal Window, and you can
+#  copy the command for activating the right environment.
+#
+#  Example: . /Users/mkaempf/opt/anaconda3/bin/activate && conda activate /Users/mkaempf/opt/anaconda3/envs/p4te;
+#
+. /Users/mkaempf/opt/anaconda3/bin/activate && conda activate /Users/mkaempf/opt/anaconda3/envs/p4te;
 
-pip install -r ./../requirements.txt
