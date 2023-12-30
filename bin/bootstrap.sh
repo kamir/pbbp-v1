590b3ace9c7aa4d70b2b301f67e6bce0fd2c81c2
Mirko Kämpf <mirko.kaempf@gmail.com>
2023-12-30 11:16:02 +0100

    prep v1

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-source ./cfg/env.sh
+source ./../cfg/env.sh
 
-source $BOT_BASE_DIR/init-venv.sh
+source $BOT_BASE_DIR/bin/init-venv.sh
 
-pip install -r $BOT_BASE_DIR/requirements.txt
+pip install -r $BOT_BASE_DIR/assistants/requirements.txt
 
 ### Prepare Assistant modules, working on MY-MEMORY
-cd assisstent
-git submodule add https://github.com/kamir/opi-bot-poc $BOT_BASE_DIR/assistent/opi-bot-poc
+git submodule add https://github.com/kamir/opi-bot-poc $BOT_BASE_DIR/assistants/opi-bot-poc
 git submodule update --init --recursive
 
 ### Prepare MY-MEMORY
 git submodule add https://github.com/kamir/my-memories-mk-v1-001 $BOT_BASE_DIR/data-stage
 git submodule update --init --recursive
 
+mkdir $BOT_BASE_DIR/data-stage/
 mkdir $BOT_BASE_DIR/data-stage/llama-index
 mkdir $BOT_BASE_DIR/data-stage/raw-data-temp
 mkdir $BOT_BASE_DIR/data-stage/raw-data-stage
---
+++ bin/clean.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+source ./../cfg/env.sh
+
+source $BOT_BASE_DIR/bin/init-venv.sh
+
+echo "--------------------------------------"
+echo "Please execute the following commands:"
+echo "--------------------------------------"
+echo
+echo "rm $BOT_BASE_DIR/.gitmodules"
+echo "rm -rf $BOT_BASE_DIR/assistants/opi-bot-poc"
+echo "rm -rf $BOT_BASE_DIR/data-stage/"
+echo
+echo "!!! WARNING !!!"
+echo
+echo "--> Make sure you have stored your data in a secure place!"
+echo
+echo "*** THERE IS NO UNDO ACTION ***"
+echo
