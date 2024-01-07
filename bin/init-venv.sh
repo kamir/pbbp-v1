8f4704754572db8ccc3e4d9c1a9667a0bd693912
Mirko Kämpf <mirko.kaempf@gmail.com>
2024-01-07 14:38:51 +0100

    prep for synch

--- bin/init-venv.sh
+++ bin/init-venv.sh
@@ -7,5 +7,9 @@
 #
 #  Example: . /Users/mkaempf/opt/anaconda3/bin/activate && conda activate /Users/mkaempf/opt/anaconda3/envs/p4te;
 #
+export ENVIRONMENT_PATH=/Users/mkaempf/opt/anaconda3/envs/opi-bot-v2-dev
+
+echo "> Activate PYTHON V-ENV in path: $ENVIRONMENT_PATH"
+
 . /Users/mkaempf/opt/anaconda3/bin/activate && conda activate /Users/mkaempf/opt/anaconda3/envs/opi-bot-v2;
 
