dd0b2300d793aca5a7f18092f38f6ed980081f4f
Mirko Kämpf <mirko.kaempf@gmail.com>
2024-01-07 14:36:17 +0100

    verbosity in exec.sh increased

--- bin/exec.sh
+++ bin/exec.sh
@@ -7,4 +7,8 @@ source $BOT_BASE_DIR/bin/init-venv.sh
 #
 # We use some of the OPI-BOT-COMPONENTS
 #
+echo $BOT_BASE_DIR
+echo "START COMMAND:"
+echo "--------------"
+echo ""$BOT_BASE_DIR/assistants/opi-bot-poc/components/asynch-event-api/exec.sh $BOT_BASE_DIR/assistants/opi-bot-poc/components/asynch-event-api""
 source $BOT_BASE_DIR/assistants/opi-bot-poc/components/asynch-event-api/exec.sh $BOT_BASE_DIR/assistants/opi-bot-poc/components/asynch-event-api
